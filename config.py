ec0b1e6a1875e9edd9d20edb1ae0aaee2c08fe78
unknown <ed.bullen@gmail.com>
2018-09-07 15:08:06 +0100

    initial base web app working

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'its_a_secret123'
+    POSTGRES_URL = 'postgresql://{}:{}@{}:{}/{}'
+
+    POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+    POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
+    POSTGRES_DB = os.environ.get('POSTGRES_DB')
+    POSTGRES_USER = os.environ.get('POSTGRES_USER')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        POSTGRES_URL.format(POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB)
+        #'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['noreply@dummy.com']
+    POSTS_PER_PAGE = 3
---
+++ env.sh
@@ -0,0 +1,16 @@
+export FLASK_APP=base.py
+echo "FLASK_APP is $FLASK_APP"
+
+export POSTGRES_HOST="localhost"
+export POSTGRES_PORT="5432"
+export POSTGRES_DB="base"
+export POSTGRES_USER="manager"
+
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME=edbullen.app@gmail.com	
+#export MAIL_PASSWORD=
+echo "Please manually set MAIL_PASSWORD"
+echo "Please manually set POSTGRESS_PASSWORD"
+
